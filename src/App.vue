94f27ed75bc1eac0b15b4a4f51488f554bac5e98
dannishah <dannnishah24@gmail.com>
2023-09-12 08:30:55 -0700

    add your all files

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+<VApp>
+
+<Navv />
+
+<Home />
+
+<Footer />
+
+</VApp>
+
+<VMain>
+
+</VMain>
+
+</template>
+
+<script setup>
+import Navv from "./components/Navv.vue"
+import Home from './components/Home.vue'
+import Footer from "./components/Footer.vue"
+
+</script>
